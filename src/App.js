df4acf2d1bea23b2444dbe0836ae44370e65dabe
bibliobee <97406629+bibliobee@users.noreply.github.com>
2023-01-12 12:13:08 -0800

    Added the Github repo url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This project is coded by Abby and is
+        <a
+          href="https://github.com/bibliobee/shecodes-react-week5"
+          target="_blank"
+        >
+          open sourced on Github
+        </a>
+        .
+      </footer>
+      >
+    </div>
+  );
 }
-
 export default App;
